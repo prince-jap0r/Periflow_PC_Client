017d9af50ea583eb5c1720dfd9f0ebddaba7e660
Prince Japor <122855521+prince-jap0r@users.noreply.github.com>
2026-04-12 03:53:01 +0600

    Delete Periflow_PC_Client directory

--- Periflow_PC_Client/main.py
+++
@@ -1,5 +0,0 @@
-from periflow.ui import run
-
-
-if __name__ == "__main__":
-    run()
--- Periflow_PC_Client/periflow/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .audio import AudioService
-from .control import ControlService
-from .video import VideoService
-
-__all__ = ["AudioService", "ControlService", "VideoService"]
